78e6875c4fc2d105c956b3d5a9dd78af8bd7bea9
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-02-12 18:45:50 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	while (true) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) {
+			break;
+		}
+		cout << a + b << endl;
+		
+	}
+}
