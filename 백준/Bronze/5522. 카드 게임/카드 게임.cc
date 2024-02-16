e666cc438e4cc08d5183fcdfda3a55e4fbf64298
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-02-16 19:15:57 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	
+	int a, b, c, d, e;
+	
+		cin >> a >> b >> c >> d >> e;
+		cout << a + b + c + d + e<<endl;
+	
+}
