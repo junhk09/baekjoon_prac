b84b8abbe2c1f8b0a0626d1b63c55726475876f6
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-03-20 19:34:18 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+	long long int a, b,c;
+	
+	cin >> a >> b>>c;
+	cout << a+b+c;
+	
+
+}
