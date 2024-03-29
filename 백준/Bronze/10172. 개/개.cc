e7df1e2338763c27f6700543440f87bf0b0dd29b
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-03-30 00:40:50 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+	
+}
