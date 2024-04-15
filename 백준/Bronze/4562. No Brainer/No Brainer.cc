a89733c9bd9bc1d4f47876b9f8e0900fd5b099a8
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-04-15 13:37:35 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	int n,a,b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		if (a < b) {
+			cout << "NO BRAINS" << '\n';
+		}
+		else {
+			cout << "MMM BRAINS" << '\n';
+		}
+	}
+}
