93f7a2a68ab072ce1195ec694353b889a2590b1f
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-04-23 14:45:24 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+}
