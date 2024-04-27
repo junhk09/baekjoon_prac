411e6642a10ec8e9cf1fc432fa550ac1aba8b54f
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-04-27 14:10:34 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+	int a, b,res;
+	cin >> a >> b;
+
+	res = 2 * b - a;
+	cout << res;
+
+
+	}
+
+
+
