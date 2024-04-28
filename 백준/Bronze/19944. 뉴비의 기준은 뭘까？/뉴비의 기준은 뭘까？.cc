52904936d9a3171effe141fab213e9d960144900
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-04-28 17:09:10 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if (m == 1 || m == 2) cout << "NEWBIE!\n";
+    else if (2 < m && m <= n) cout << "OLDBIE!\n";
+    else cout << "TLE!\n";
+
+	}
+
+
+
