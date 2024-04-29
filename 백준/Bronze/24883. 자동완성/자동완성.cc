868fc25acedf0e0228d124f3f38229c94e9b04dc
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-04-29 15:49:02 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	char n;
+	cin >> n;
+	if (n == 'n' || n == 'N') cout << "Naver D2";
+	else cout << "Naver Whale";
+
+
+}
