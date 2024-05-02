03b3d817c5a7ba9a73a063492e83f629daa90515
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-05-02 21:27:54 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+using namespace std;
+int main() {
+	int n=0;
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		cout << s[i];
+		n++;
+		if (n == 10) {
+			cout << endl;
+			n = 0;
+		}
+
+	}
+
+
+
+
+
+	}
+
+
+
