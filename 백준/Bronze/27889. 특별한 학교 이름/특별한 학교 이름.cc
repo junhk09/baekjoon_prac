aedc64365828333888f81dd5f0e65d03838eee4b
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-05-06 17:40:44 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    if (s[0] == 'N') cout << "North London Collegiate School";
+    if (s[0] == 'B') cout << "Branksome Hall Asia";
+    if (s[0] == 'K') cout << "Korea International School";
+    if (s[0] == 'S') cout << "St. Johnsbury Academy";
+  
+    
+}
