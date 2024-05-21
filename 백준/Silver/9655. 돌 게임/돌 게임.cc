3dc5f530e67ee07cbdb736e2c887d72f08f5560c
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-05-21 15:57:19 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <numeric>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	if (n % 2 == 0) {
+		cout << "CY";
+	}
+	else {
+		cout << "SK";
+	}
+	
+
+
+
+}
