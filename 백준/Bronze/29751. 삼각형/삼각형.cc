5df1a13b5f34716d5d311061bba4b8fa502fc24b
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-05-28 19:36:49 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <numeric>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int w, h;
+	cin >> w >> h;
+
+	printf("%.1lf", (double)w * h / 2);
+
+}
