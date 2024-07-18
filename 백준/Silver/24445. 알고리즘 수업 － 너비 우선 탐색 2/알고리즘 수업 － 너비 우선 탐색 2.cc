bb8101e688e5985c809f7a46181b6548df31f1da
junhk09 <147141178+junhk09@users.noreply.github.com>
2024-07-18 17:05:25 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 104 ms, Memory: 9228 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <numeric>
+#include <map>
+using namespace std;
+vector<int>gf[100005];
+int visited[100005];
+int ranking[100005];
+int res = 1;
+void bfs(int a) {
+	queue<int>q;
+	q.push(a);
+	visited[a] = 1;
+	ranking[a] = res++;
+	while (!q.empty()) {
+		int save = q.front();
+		q.pop();
+		for (int i = 0; i < gf[save].size(); i++) {
+			int x = gf[save][i];
+			if (visited[x] == 0) {
+				ranking[x] = res++;
+				q.push(x);
+				visited[x] = 1;
+			}
+		}
+
+
+
+	}
+	
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, m, r;
+	cin >> n >> m >> r;
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		cin >> x >> y;
+		gf[x].push_back(y);
+		gf[y].push_back(x);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(gf[i].begin(), gf[i].end(),greater<int>());
+	}
+
+	bfs(r);
+
+	for (int i = 1; i <= n; i++) {
+		cout << ranking[i] << '\n';
+	}
+
+
+	return 0;
+
+
+}
