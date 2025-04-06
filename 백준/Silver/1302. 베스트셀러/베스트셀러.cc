62cd129e25dd525944618979b8752ed9d1689554
junhk09 <147141178+junhk09@users.noreply.github.com>
2025-04-06 16:42:00 +0900

    [Silver IV] Title: 베스트셀러, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.cc
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.cc
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <algorithm>
-#include <map>
+#include <stack>
 #include <vector>
 #include <string>
+#include <queue>
+#include <deque>
+#include <numeric>
+#include <map>
 using namespace std;
-
 bool cmp(const pair<string, int>& a, const pair<string, int>& b) {
     if (a.second == b.second) return a.first < b.first;
     return a.second > b.second;
@@ -13,22 +16,20 @@ bool cmp(const pair<string, int>& a, const pair<string, int>& b) {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
     int n;
     cin >> n;
-
     map<string, int> m;
 
     for (int i = 0; i < n; i++) {
         string s;
         cin >> s;
-        m[s]++; // 기존에 없으면 0에서 시작, 자동으로 +1
+        m[s]++; 
     }
 
     vector<pair<string, int>> v(m.begin(), m.end());
     sort(v.begin(), v.end(), cmp);
 
-    // 가장 많이 나온 단어 출력
+    
     cout << v[0].first << '\n';
 
     return 0;
