f4aa8d48839cc441c91e507d7bf500af30332a2b
junhk09 <147141178+junhk09@users.noreply.github.com>
2025-07-21 16:23:51 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <numeric>
+#include <map>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    cout << n * (n - 1);
+
+
+
+    return 0;
+}
